a1d8acaf72b6398e89625708695c58b09493aa37
toxic-bishop <okanlawon.daniel@yahoo.com>
2022-04-19 11:47:28 +0100

    size, text styles and color

---
+++ lib/src/core/widgets/dummy_widgets/app_size.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class AppSize {
+  AppSize._();
+
+  static const double defaultAppBarHeight = 56.0;
+
+  // vertical space smaller than default
+  static const verticalSpaceSmall = SizedBox(height: 8.0);
+  static const verticalSpaceMedium = SizedBox(height: 16.0);
+  static const verticalSpaceLarge = SizedBox(height: 24.0);
+
+  // horizontal space smaller than default
+  static const horizontalSpaceSmall = SizedBox(height: 8.0);
+  static const horizontalSpaceMedium = SizedBox(height: 16.0);
+  static const horizontalSpaceLarge = SizedBox(height: 24.0);
+}
