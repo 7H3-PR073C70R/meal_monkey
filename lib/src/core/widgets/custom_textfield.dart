3c765653b95753b5fb2209dcbfeab40a7b7bdfa9
github_aliyukabir1 <aliyukabirlawal@gmail.com>
2022-04-20 08:04:50 +0100

    reusable components

---
+++ lib/src/core/widgets/custom_textfield.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:meal_monkey/src/core/widgets/dummy_widgets/app_textstyles.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController? controller;
+  final String? initialValue;
+  final String? hintText;
+  final IconData? prefixIcon;
+  const CustomTextField({
+    Key? key,
+    this.controller,
+    this.hintText,
+    this.initialValue,
+    this.prefixIcon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      initialValue: initialValue,
+      cursorHeight: 25,
+      cursorColor: Colors.black,
+      decoration: InputDecoration(
+        prefixIcon: Icon(
+          prefixIcon,
+          size: 25,
+        ),
+        hintText: hintText,
+        hintStyle: AppTextStyle.headerTwoTextStyle,
+        filled: true,
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 25, horizontal: 40),
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(35),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/core/widgets/popular_restaurant_card.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:meal_monkey/src/core/constants/app_colors.dart';
+import 'package:meal_monkey/src/core/widgets/dummy_widgets/app_size.dart';
+import 'package:meal_monkey/src/core/widgets/dummy_widgets/app_textstyles.dart';
+
+class PopularRestaurantCard extends StatelessWidget {
+  final double? rating;
+  final int? numberOfRatings;
+  final String imageUrl;
+  final void Function()? ontap;
+
+  const PopularRestaurantCard({
+    Key? key,
+    this.rating,
+    this.numberOfRatings,
+    required this.imageUrl,
+    this.ontap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: ontap,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(imageUrl),
+              ),
+            ),
+            height: MediaQuery.of(context).size.height * 0.23,
+          ),
+          AppSize.verticalSpaceSmall,
+          Text(
+            'Minute by tuk tuk',
+            style: AppTextStyle.headerTwoTextStyle,
+          ),
+          const SizedBox(height: 5),
+          Row(
+            children: [
+              const Icon(
+                Icons.star,
+                color: AppColors.orange,
+              ),
+              const SizedBox(width: 2),
+              Text('$rating', style: const TextStyle(color: AppColors.orange)),
+              const SizedBox(width: 5),
+              Text(
+                '($numberOfRatings ratings)  Cafe',
+                style: const TextStyle(color: Colors.grey),
+              ),
+              const SizedBox(width: 5),
+              Container(
+                height: 5,
+                width: 5,
+                decoration: const BoxDecoration(
+                  color: AppColors.orange,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              AppSize.horizontalSpaceSmall,
+              const Text(
+                'Western Food',
+                style: TextStyle(color: Colors.grey),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
