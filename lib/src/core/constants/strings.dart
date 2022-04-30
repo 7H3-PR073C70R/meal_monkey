4af7ffb49b07a2a0517ecd3e48ad36f98978bb9b
Badiru <55401789+Badiru27@users.noreply.github.com>
2022-04-30 16:22:51 +0100

    Implement more and notification UI

--- lib/src/core/constants/strings.dart
+++ lib/src/core/constants/strings.dart
@@ -3,10 +3,18 @@ class AppString {
   static const desert = 'Desert';
   static const ratings = 'ratings';
   static const viewAll = 'View all';
-  static const recentItems = 'Recents Items';
+  static const recentItems = 'Recent Items';
   static const mostPopular = 'Most Popular';
   static const popularResturants = 'Popular Restaurants';
   static const searchFood = 'Search food';
   static const currentLocation = 'Current Location';
   static const deliverTo = 'Deliver to';
+
+
+  static const more = 'More';
+  static const paymentDetails = 'Payment Details';
+  static const myOrder = 'My Order';
+  static const notification = 'Notifications';
+  static const inbox = 'Inbox';
+  static const aboutUs = 'About Us';
 }
